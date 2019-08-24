f2ac776802e5ff9c22e30f2b23b3fd2ae45c8923
doeringp <20394732+doeringp@users.noreply.github.com>
2019-08-24 12:20:03 +0200

    replaced field Age with BirthDate.

--- backend/src/ApplicationCore/Models/Ticket.cs
+++ backend/src/ApplicationCore/Models/Ticket.cs
@@ -15,7 +15,7 @@ namespace EventManagement.ApplicationCore.Models
         public bool? TermsAccepted { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
-        public int? Age { get; set; }
+        public DateTime? BirthDate { get; set; }
         public string Address { get; set; }
         public string RoomNumber { get; set; }
         public DateTime CreatedAt { get; set; }
