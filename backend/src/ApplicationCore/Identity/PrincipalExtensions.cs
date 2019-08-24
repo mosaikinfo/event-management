11a4cb6756423ad3f48182c70c7742299b306ba3
doeringp <20394732+doeringp@users.noreply.github.com>
2019-08-24 11:38:50 +0200

    fixed setting author info when using client only auth.

--- backend/src/ApplicationCore/Identity/PrincipalExtensions.cs
+++ backend/src/ApplicationCore/Identity/PrincipalExtensions.cs
@@ -1,11 +1,24 @@
-﻿using IdentityServer4.Extensions;
+﻿using IdentityModel;
+using IdentityServer4.Extensions;
 using System;
+using System.Security.Claims;
 using System.Security.Principal;
 
 namespace EventManagement.Identity
 {
     public static class PrincipalExtensions
     {
+        /// <summary>
+        /// Returns true, if the authenticated identity is a real person.
+        /// Returns false, if it is only an API Client (S2S App) without user context.
+        /// </summary>
+        /// <param name="principal">The authenticated user</param>
+        public static bool IsPerson(this IPrincipal principal)
+        {
+            var id = principal.Identity as ClaimsIdentity;
+            return id.FindFirst(JwtClaimTypes.Subject) != null;
+        }
+
         /// <summary>
         /// Get the current user id.
         /// </summary>
