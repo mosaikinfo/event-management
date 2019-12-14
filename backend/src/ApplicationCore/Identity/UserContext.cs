b527557f94b98f181c8f536b3d25e84351352be1
doeringp <doeringp@users.noreply.github.com>
2019-12-14 22:14:19 +0100

    methods to find out the current user context.

---
+++ backend/src/ApplicationCore/Identity/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EventManagement.Identity
+{
+    /// <summary>
+    /// Information about the context in which the current user is currently working.
+    /// </summary>
+    public class UserContext
+    {
+        /// <summary>
+        /// Id of the current user.
+        /// </summary>
+        public Guid UserId { get; set; }
+
+        /// <summary>
+        /// Id of the current event.
+        /// </summary>
+        public Guid? EventId { get; set; }
+    }
+}
