e4d63379084b47cdb7ffc180d21daa8f0974956f
doeringp <20394732+doeringp@users.noreply.github.com>
2021-04-26 22:11:14 +0200

    Dockerize the application (#43)
    * Created a Dockerfile
    
    * Memory limits for containers
    
    * Fixed time zone error under linux
    
    * Fixed reading the logo image for pdf generation
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Fixed path in dockerfile
    
    * Fixed another path in dockerfile
    
    * Use build number as image tag
    
    Co-authored-by: doeringp <doeringp@users.noreply.github.com>

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+FROM mcr.microsoft.com/dotnet/sdk:3.1-buster AS build
+
+# Install NodeJS 10.x and NPM to build the Angular app
+RUN curl -sL https://deb.nodesource.com/setup_10.x | bash - \
+    && apt-get install -y nodejs
+
+# Install node_modules
+WORKDIR /source/backend/src/Web/ClientApp/
+COPY backend/src/Web/ClientApp/*.json ./
+RUN npm install
+
+# copy csproj and restore as distinct layers
+WORKDIR /source
+COPY backend/src/ApplicationCore/*.csproj ./backend/src/ApplicationCore/
+COPY backend/src/Identity/*.csproj ./backend/src/Identity/
+COPY backend/src/Infrastructure/*.csproj ./backend/src/Infrastructure/
+COPY backend/src/Web/*.csproj ./backend/src/Web/
+COPY ticket-generation/src/TicketGeneration/*.csproj ./ticket-generation/src/TicketGeneration/
+RUN dotnet restore ./backend/src/Web/Web.csproj
+
+# copy everything else and build app
+COPY . ./
+RUN dotnet publish ./backend/src/Web/Web.csproj -c Release -o /app --no-restore
+
+FROM mcr.microsoft.com/dotnet/aspnet:3.1-buster-slim
+WORKDIR /app
+RUN apt-get update && apt-get install -y \
+  # required for QR code generation
+  libgdiplus \
+  && rm -rf /var/lib/apt/lists/*
+COPY --from=build /app ./
+ENTRYPOINT ["dotnet", "EventManagement.WebApp.dll"]
--- backend/src/ApplicationCore/TicketGeneration/PdfTicketService.cs
+++ backend/src/ApplicationCore/TicketGeneration/PdfTicketService.cs
@@ -2,21 +2,25 @@
 using EventManagement.ApplicationCore.Models;
 using EventManagement.ApplicationCore.Tickets;
 using EventManagement.TicketGeneration;
+using Microsoft.AspNetCore.Hosting;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using TimeZoneConverter;
 
 namespace EventManagement.ApplicationCore.TicketGeneration
 {
     public class PdfTicketService : IPdfTicketService
     {
         private readonly ITicketsRepository _tickets;
+        private readonly IWebHostEnvironment _hostingEnvironment;
 
-        public PdfTicketService(ITicketsRepository tickets)
+        public PdfTicketService(ITicketsRepository tickets, IWebHostEnvironment hostingEnvironment)
         {
             _tickets = tickets;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         public async Task<Stream> GeneratePdfAsync(Guid ticketId, string ticketValidationUriFormat)
@@ -42,18 +46,18 @@ namespace EventManagement.ApplicationCore.TicketGeneration
                 ticket.TicketSecret);
 
             // TODO: configure logo in event settings.
-            string logoUrl = new Uri(new Uri(validationUri), "/mosaik.png").AbsoluteUri;
+            string logoPath = _hostingEnvironment.WebRootFileProvider.GetFileInfo("mosaik.png").PhysicalPath;
 
             // TODO: configure timezone in event settings.
-            // (UTC+01:00) Amsterdam, Berlin, Bern, Rom, Stockholm, Wien
-            var timezone = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+            // See https://devblogs.microsoft.com/dotnet/cross-platform-time-zones-with-net-core/
+            var timezone = TZConvert.GetTimeZoneInfo("Europe/Berlin");
             DateTime startTime = TimeZoneInfo.ConvertTimeFromUtc(ticket.Event.StartTime, timezone);
 
             // TODO: make date time format configurable in event settings.
             var values = new TicketData
             {
                 EventName = ticket.Event.Name,
-                EventLogo = logoUrl,
+                EventLogo = logoPath,
                 TicketId = ticket.TicketNumber,
                 QrValue = validationUri,
                 Host = ticket.Event.Host,
